1dcfc1a3cf8ea301373e8e325787aa7ad58ca531
Hong Phuc <og.viet.003.dev@gmail.com>
2020-02-14 16:17:23 +0700

    Create counter component - finish

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import './Counter.scss';
 
 class Counter extends Component {
+  state = { count: 0 };
+
+  increment = () => this.setState({ count: this.state.count + 1 });
+  decrement = () => this.setState({ count: this.state.count - 1 });
+
   render() {
     return (
-      //render here
+      <div className="counter">
+        <h2>Counter</h2>
+
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span className="count">{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
     )
   }
 }
