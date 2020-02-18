fe275f1b8cfa00164ae896ab84020c9d43ea01c6
Hong Phuc <og.viet.003.dev@gmail.com>
2020-02-18 15:01:47 +0700

    Apply middleware using redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import counterReducer from './reducers/counterReducer';
 
 const store = createStore(
   counterReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
 );
 
 export default store;
